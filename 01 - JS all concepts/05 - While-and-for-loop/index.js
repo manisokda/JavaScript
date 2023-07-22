a93b01a8109ef7006dc9a6828f6a8199063cb7cf
manisokda <manisokda@gmail.com>
2023-07-22 16:39:34 +0530

    loop problems

--- 01 - JS all concepts/05 - While-and-for-loop/index.js
+++ 01 - JS all concepts/05 - While-and-for-loop/index.js
@@ -1,7 +1,21 @@
 // 1. Print all numbers from 1 to 5
+let input = prompt("Provide sample input ");
 let i = 1;
-while (i <= 5) {
-    console.log(i);
+let result = '';
+while (i <= input) {
+    result += i + ' ';
     i++;
 }
-// output is - 1 2 3 4 5
+console.log(result); // output is - 1 2 3 4 5
+
+
+
+// 2. Print all numbers from down to top
+let input = prompt("Provide sample input ");
+let i = input;
+let result = '';
+while (i >= 1) {
+    result += i + ' ';
+    i--;
+}
+console.log(result); // output - 5 4 3 2 1
